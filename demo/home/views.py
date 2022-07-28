89f4eb90b207dabfc9f04b65613e2d4bcb9c9571
adityasimant <adityasimant88@gmail.com>
2022-07-28 14:03:53 +0530

    commit

--- demo/home/views.py
+++ demo/home/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse(" this is homepage ")
+    # return HttpResponse(" this is homepage ")
+    return render(request, 'index.html')
 def about(request):
     return HttpResponse(" this is about page ")
 def menu(request):
