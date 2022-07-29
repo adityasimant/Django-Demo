a18987d5171c03379821f1ee120c11161f924d74
adityasimant <adityasimant88@gmail.com>
2022-07-29 14:05:13 +0530

    db

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# admin.site.site_header = "Aditya Admin"
+# admin.site.site_title = "Aditya Admin Portal"
+# admin.site.index_title = "Welcome to Aditya Researcher Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls') )
