be72a26d71e93eae9743ace068815672a4d4a0a6
adityasimant <adityasimant88@gmail.com>
2022-07-30 20:02:25 +0530

    made migrations

--- demo/home/models.py
+++ demo/home/models.py
@@ -1,3 +1,10 @@
+import email
+from statistics import mode
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Contacts(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    desc = models.TextField()
